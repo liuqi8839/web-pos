d19196d4676699217410544a946555778c4a2e9e
liuqi8839 <liuqitiger@qq.com>
2014-09-02 11:49:47 +0800

    refactor itemlist

--- app/scripts/itemlist.js
+++ app/scripts/itemlist.js
@@ -2,87 +2,44 @@ $(document).ready(function() {
     var kind,price,name,unit;
     var numbers = 0;
     if(!localStorage.Item) {
-        localStorage.Item = "[]";
+        localStorage.Item = '[]';
     }
-    var Items = JSON.parse(localStorage.Item);
+    var goods = JSON.parse(localStorage.Item);
 
-    for(var i = 0 ; i < Items.length; i++) {
-        numbers += Items[i].numbers;
+    for(var i = 0 ; i < goods.length; i++) {
+        numbers += goods[i].numbers;
     }
-    $("#item-numbers").text(numbers);
+    $('#item-numbers').text(numbers);
 
-    function addItem(kind,name,price,unit) {
+    function addGoods(name) {
         numbers += 1;
-        $("#item-numbers").text(numbers);
+        $('#item-numbers').text(numbers);
         var flag = true;
-        for (var i = 0; i < Items.length; i++){
-            if(Items[i].name == name) {
-                Items[i].numbers += 1;
+        for (var i = 0; i < goods.length; i++){
+            if(goods[i].name == name) {
+                goods[i].numbers += 1;
                 flag = false;
                 break;
             }
         }
         if(flag == true) {
-            Items.push({kind:kind,name: name, price: price, unit: unit, numbers: 1});
+            goods.push({name: name, numbers: 1});
         }
-        localStorage.Item = JSON.stringify(Items);
+        localStorage.Item = JSON.stringify(goods);
+    }
+    var allItems = JSON.parse(localStorage.AllItems);
+    for(var j = 0; j < allItems.length; j++) {
+        name = allItems[j].name;
+        kind = allItems[j].kind;
+        price = allItems[j].price;
+        unit = allItems[j].unit;
+        var addItem = $("<tr><td>"+kind+"</td><td>"+name+"</td><td>"+price+"</td><td>"+unit+"</td><td><p align='center'><a href='#' class='btn btn-primary btn-2' role='button'>加入购物车</a></p></td></tr>");
+        $('tbody').append(addItem);
     }
 
-    $(".cola").on("click", function(event) {
+    $('a').on('click', function(event) {
         event.preventDefault();
         event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
+        addGoods($(this).parent().parent().prev().prev().prev().text());
     });
-    $(".spring").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
-    });
-    $(".apple").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
-    });
-    $(".litchi").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
-
-    });
-    $(".battery").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
-    });
-    $(".noodle").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        kind = $(this).data("kind");
-        price = +$(this).data("price");
-        name = $(this).data("name");
-        unit = $(this).data("unit");
-        addItem(kind,name,price,unit);
-    });
-
-
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,4 +9,43 @@ $(document).ready(function() {
         numbers += Items[i].numbers;
     }
     $("#item-numbers").text(numbers);
+    var ItemList = [
+        {
+            kind:'饮料',
+            name:'可口可乐',
+            price:3,
+            unit:'瓶'
+        },
+        {
+            kind:'饮料',
+            name:'雪碧',
+            price:3,
+            unit:'瓶'
+        },
+        {
+            kind:'水果',
+            name:'苹果',
+            price:5.5,
+            unit:'斤'
+        },
+        {
+            kind:'水果',
+            name:'荔枝',
+            price:15,
+            unit:'斤'
+        },
+        {
+            kind:'生活用品',
+            name:'电池',
+            price:2,
+            unit:'个'
+        },
+        {
+            kind:'食品',
+            name:'方便面',
+            price:4.5,
+            unit:'袋'
+        }
+        ];
+    localStorage.AllItems = JSON.stringify(ItemList);
 });
